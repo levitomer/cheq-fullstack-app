f4fd2514583ff3b857cf9f6a9d720013496979f6
Tomer Levi <levi.tomer2@gmail.com>
2020-03-18 17:27:32 +0200

    db mocks
    + mocks for testing endpoints responses

---
+++ server/db/edit-vast.js
@@ -0,0 +1,8 @@
+export default {
+    id: 2,
+    vast_url:
+        'https://gist.github.com/yosiweinreb/5718777d80e3a00d2567b3836eaf6323',
+    position: 'top_middle',
+    width: 23,
+    height: 54
+};
